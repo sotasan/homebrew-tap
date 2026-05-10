2e2839de47bbd1faca8596f7bc2e627219d254f3
Sōta <sota@sota.moe>
2026-05-10 20:08:32 +0200

    feat: add livecheck for bump-packages action

--- Casks/piyo.rb
+++ Casks/piyo.rb
@@ -8,6 +8,11 @@ cask "piyo" do
   desc "The coziest terminal emulator"
   homepage "https://piyo.sh/"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   auto_updates true
   depends_on arch: :arm64
 
